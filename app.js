5e9b2bfbe27f01333dcf79d7a479f1ca3b6c80a0
Jon Hammond <jonh1016@gmail.com>
2016-02-18 11:24:49 -0700

    made node app to respond with json data for a given imdb id

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-var http = require'http';
-var imdbID = process.argv[3];
+var http = require('http');
+var request = require('request');
+
+var imdbID = process.argv[2];
+var url = 'http://www.omdbapi.com/?i=' + imdbID + '&plot=short&r=json';
 var port = 8000;
 
 var server = http.createServer(requestHandler);
 
 function requestHandler (req, res) {
-
+  request(url, function (error, response, body) {
+    if (error) {
+      res.end('Something went wrong (check the imdb id)');
+    }
+    else {
+      res.setHeader('Content-Type', 'application/json');
+      // res.write('<ul><li>'+ imdbID + '<li><ul>');
+      res.end(response.body);
+    }
+  });
 }
 
+server.listen(port, function() {
+  console.log("Server is running on http://localhost:", port);
+});
